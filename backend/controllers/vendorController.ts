d36fe67e55a385d540620f373dc597a73cb1d541
Manish <iammanish0312@gmail.com>
2024-04-11 15:57:03 +0530

    vendors crud

--- backend/controllers/vendorController.ts
+++ backend/controllers/vendorController.ts
@@ -1,5 +1,9 @@
 import { Request, Response } from "express";
-import { vendorLoginInput } from "../dto/vendor";
+import {
+  editVendorDetails,
+  editVendorService,
+  vendorLoginInput,
+} from "../dto/vendor";
 import { Vendor } from "../models/vendor";
 import { comparePassword, generateToken } from "../utils/passwordHashing";
 
@@ -35,4 +39,65 @@ const vendorLogin = async (req: Request, res: Response) => {
   }
 };
 
-export { vendorLogin };
+const vendorProfile = async (req: Request, res: Response) => {
+  try {
+    const user = req.user;
+    if (user) {
+      const data = await Vendor.findById({ _id: user._id });
+      res.status(200).json({
+        success: true,
+        message: "User profile",
+        profile: data,
+      });
+    }
+  } catch (err) {
+    res
+      .status(500)
+      .json({ success: false, msessage: `Internal sever error ${err}` });
+  }
+};
+
+const editVendorDetails = async (req: Request, res: Response) => {
+  try {
+    const { name, phone, address, foodType } = <editVendorDetails>req.body;
+    const user = await Vendor.findByIdAndUpdate(
+      req.user._id,
+      {
+        name,
+        phone,
+        address,
+        foodType,
+      },
+      { new: true }
+    );
+    return res
+      .status(200)
+      .json({ success: true, message: "user data updated", user });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ success: false, msessage: `Internal sever error ${err}` });
+  }
+};
+
+const editVendorService = async (req: Request, res: Response) => {
+  try {
+    const { serviceAvailable } = <editVendorService>req.body;
+    const user = await Vendor.findByIdAndUpdate(
+      req.user._id,
+      {
+        serviceAvailable,
+      },
+      { new: true }
+    );
+    return res
+      .status(200)
+      .json({ success: true, message: "Service availability change", user });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ success: false, msessage: `Internal sever error ${err}` });
+  }
+};
+
+export { vendorLogin, vendorProfile, editVendorDetails, editVendorService };
